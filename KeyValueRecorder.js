4454a77a9ef8b5c0bd9ac260ce05d27ae9db75a3
Shaffan Mustafa <shaffan1996@gmail.com>
2022-08-17 15:31:18 -0400

    get endpoint returns value when queried by keys

--- KeyValueRecorder.js
+++ KeyValueRecorder.js
@@ -6,7 +6,7 @@ const PORT = 4000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let data = []
+let data = {}
 
 function fileExists(path) {
     try {
@@ -30,7 +30,7 @@ function writeToFile() {
 function getAllDataFromFile() {
     // returns JSON that we will need to read json.parse()
         if (!fileExists('output.txt')) {
-            fs.writeFileSync("output.txt", "[]");
+            fs.writeFileSync("output.txt", "{}");
         }
         data = JSON.parse(fs.readFileSync('output.txt', 'utf8'));
 }
@@ -49,7 +49,8 @@ app.get("/", (request, response) => response.status(200).send("You're at the roo
 
 app.get("/set", (req, res) => {
     let kv = getKeyAndValueFromUrl(req.url);
-    data.push(kv)
+    data[kv["key"]] = kv["value"];
+    console.log(data);
     writeToFile();
     res.status(200).json(kv);
 })
@@ -58,7 +59,7 @@ app.get("/get", (req, res) => {
     let keyFromUrl = req.url.split("?")[1];
 
 
-    let returnValue = data.find(item => item["key"] === keyFromUrl)["value"]
+    let returnValue = data[keyFromUrl];
     res.json({ "key from URL": keyFromUrl, "value": returnValue });
 })
 
